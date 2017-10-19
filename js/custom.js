4cfaf18af28180898b1d674bb1f03564cc6f0507
webdev498 <gcmobile@mail.com>
2017-10-19 09:54:12 -0400

    Score boarding update

--- js/custom.js
+++ js/custom.js
@@ -1,15 +1,8 @@
 let isNetworkOnline = false;
 
-let ROUND_CURRENT_INDEX = 1;
-let Answer_Pass_Index = 0;
 let QuizDetail;
-let canGoNext = false;
 let totalCorrectNum = 0;
 let current_round_correct_num = 0;
-
-let currentRoundAnswerIDs = [ 0, 0, 0, 0 ];
-
-
 
 const arrayMenuForGSK = [
     {img: 'answer_none.png', text: 'NO VACCINATION REQUIRED' },
@@ -72,6 +65,9 @@ refreshBoardWithInfo = (qInfo) => {
 
     //Init dropbox
     dropMenu = new cbpTooltipMenu( document.getElementById( 'answer-container' ) );
+
+    //Save state
+    saveCurrentState();
 }
 
 /* Create the dropdown menu */
@@ -148,6 +144,10 @@ onSelectedAnswer = ($answerEl, answerIndex, imgSrc) => {
     if (checkResult == -1) { 
         $('.pt-btn-check').removeClass('pt-btn-check-inactive');
     }
+
+    //Save state
+    saveCurrentState();
+
 }
 
 /* Check the answer result */
@@ -161,8 +161,19 @@ checkAnswersResult = () => {
 
     let correctNum = 0;
     for (i = 0; i < countAnswerCountsPerRound; i ++) {
-        if (answerArray[i] != correctAnswerIndexes[i]) break;
-        correctNum ++;
+        if (answerArray[i] == correctAnswerIndexes[i]) correctNum ++;
+
+        //Post answer result to server or localstorage
+        score = 100;
+        questionInfo = QuizDetail['Activity129']['Question' + ((i * 1 + 1) + (currentRound * countAnswerCountsPerRound))];       
+        questionID = questionInfo[0][1];
+        answerID = questionInfo[5][1].split('&&')[answerArray[i]].split('||')[0];
+        if (navigator.onLine) {  //Sending to server
+            sendReportToServer(questionID, answerID, score);
+        }
+        else { //Store it in local storage
+            storeReportToLocalStorage(questionID, answerID, score);
+        }
     }
 
     if (correctNum == countAnswerCountsPerRound)
@@ -211,12 +222,12 @@ $(document ).ready(function() {
     //At first, refresh the board
     refreshBoardWithInfo();
 
-    if (navigator.onLine) {
+    // if (navigator.onLine) {
         //Send the responses reporting to server if exists
-        sendStoredReportToServer();
-    }
+    //     sendStoredReportToServer();
+    // }
 
-    if (loadLastState() == false) { //Tried to load the last state first. If failed~~~~~~~~~~~
+    // if (loadLastState() == false) { //Tried to load the last state first. If failed~~~~~~~~~~~
     
         if (navigator.onLine) { // ------------- Online mode -----------
             isNetworkOnline = true;
@@ -252,18 +263,13 @@ $(document ).ready(function() {
                     QuizDetail = $.parseJSON(activity_json);
                     //Update the screens with queries
                     updateQuestionsAndAnswers(QuizDetail['Activity129']);
-
-                    //Enable Start Button
-                    $('.pt-btn-begin').removeClass('pt-btn-begin-inactive');
-                    $('.pt-btn-begin').toggleClass('pulse-button');
-
                 }
             } else {
                 alert('Sorry! No Web Storage support..');
             }
             
         }
-    }
+    // }
 
 });
 
@@ -341,12 +347,14 @@ sendStoredReportToServer = () => {
 
 sendReportToServer = (question_id, answer_id, score) => {
     let localInfo = $.parseJSON(currentJSONString);
+
+    console.log('asdfasdfasdf', localInfo);
     let eventID = localInfo.TTInfoDictionary.TTInfo[0][1];
     let userID = localInfo.TTInfoDictionary.TTInfo[1][1];
 
     $.post("https://gsk.mc3tt.com/tabletop/activities/addactivitycompetition/", 
             { 
-                activity_id: 124,
+                activity_id: 129,
                 user_id: userID,
                 event_id: eventID,
                 question_id: question_id,
@@ -363,127 +371,15 @@ sendReportToServer = (question_id, answer_id, score) => {
             });
 }
 
-/* 
-- Check if all answers are selected
-- Calculate the score
-- Update the Answer Review screen elements
-- Post or store the answer report
-- Display the final Correct or Incorrect symbol
-*/
-checkCanGoNext = () => {
-    let quizInfo = QuizDetail['Activity124'];
-    var isAllSelected = true;
-    
-    
-    //Check if all answers are selected
-    for (index = 1; index <= 4; index ++) {
-        let questionIndex = (ROUND_CURRENT_INDEX - 1) * 4 + index;
-        let nameValue = 'answersGroup-' + questionIndex;
-        let checkedIDValue = $('input:radio[name=' + nameValue + ']:checked').val();
-
-        if (isAllSelected && checkedIDValue == 'answer-id-default-' + questionIndex){
-            //Show the alert view
-            $('#popup-alert-div').css('display', 'block');
-            isAllSelected = false;
-
-            return false;
-        }
-    }
-
-    //Calculate the correct answers
-    current_round_correct_num = 0;
-    for (index = 1; index <= 4; index ++) {
-        let questionIndex = (ROUND_CURRENT_INDEX - 1) * 4 + index;
-        let nameValue = 'answersGroup-' + questionIndex;
-        let checkedIDValue = $('input:radio[name=' + nameValue + ']:checked').val();
-        let correctAnswerID = quizInfo['Question' + questionIndex][4][1];
-        let answerID = checkedIDValue.split('-').pop();
-        let score = 0;
-        if (correctAnswerID == answerID) {
-            //Increase the total correct count
-            totalCorrectNum ++;
-
-            //Save the correct number for this round
-            current_round_correct_num ++;
-
-            //Set the score
-            score = 100;
-
-            //Remove the correct answer label
-            $targetCorrectAnswerLabel = $('#round-' + ROUND_CURRENT_INDEX + '-' + index + '-correct-answer');
-            $targetCorrectAnswerLabel.css('display', 'none');
-        }
-
-        //Update the contents on Answer Review Screen (Dropdown menu and correct answers underneath the incorrect answer)
-        let strTargetID = 'round-' + ROUND_CURRENT_INDEX + '-' + index + '-dropdown';
-        $targetDropDownAnswer = $('#' + strTargetID + '-answer');
-        nameValue = 'answersGroup-' + questionIndex + '-answer';
-        IDValue = 'answer-id-default-' + questionIndex + '-answer';
-        selectedAnswerValue = $('label[for=' + checkedIDValue + ']').text();
-
-        $newRadio = $( "<input type='radio' name='" + nameValue + "' value='" + IDValue + "' checked='checked' id='" + IDValue + "'>" );
-        $newLabel = $( "<label for='answer-id-0'>" + selectedAnswerValue + "</label>" );
-        $targetDropDownAnswer.append($newRadio);
-        $targetDropDownAnswer.append($newLabel);
-
-        //Send the report to server
-        let questionID = quizInfo['Question' + questionIndex][0][1];
-        if (navigator.onLine) {  //Sending to server
-            sendReportToServer(questionID, answerID, score);
-        }
-        else { //Store it in local storage
-            storeReportToLocalStorage(questionID, answerID, score);
-        }
-    }
-
-    if(current_round_correct_num == 4) {  //Display Correct
-        $('#pt-btn-check-result-' + ROUND_CURRENT_INDEX).toggleClass('pt-btn-check-result-correct');
-    }
-    else{ //Display Incorrect
-        $('#pt-btn-check-result-' + ROUND_CURRENT_INDEX).toggleClass('pt-btn-check-result-incorrect');
-    }
-
-    //Update the score board
-    $('.pt-score-label .number-score').text(totalCorrectNum * 100);
-
-    //Increase the answer index
-    Answer_Pass_Index ++;
-
-    //Save the state
-    saveCurrentState();
-
-    //Refresh the selected answers
-    currentRoundAnswerIDs = [0, 0, 0, 0];
-
-    return isAllSelected;
-}
-
-/* Update answer section */
-updateAnswerDropdown = (answerIDs) => {
-    if (answerIDs) {
-        $.each(answerIDs, function( index, idValue ) {
-            if (idValue == 0) return;
-            $('#round-' + ROUND_CURRENT_INDEX + '-' + (index + 1) + '-dropdown ' + idValue).prop("checked", true)
-        });
-    }
-}
-
 
 /* Save the current state */
 
 saveCurrentState = () => {
-    currentRound = Answer_Pass_Index;
-    currentCorrentNum = totalCorrectNum;
-
-    // if (currentRound >= 3) {
-    //     localStorage.removeItem("lastState");
-    //     return;
-    // }
+    currentRound = currentRound;
 
     state = {
         currentRound: currentRound,
-        currentCorrectNum: totalCorrectNum,
-        currentAnswerSelection: currentRoundAnswerIDs
+        currentAnswerSelection: answerArray
     }
     localStorage.setItem("lastState", JSON.stringify(state));
 }
@@ -502,10 +398,6 @@ loadLastState = () => {
 
         QuizDetail = $.parseJSON(activity_json);
 
-        //Enable Start Button
-        $('.pt-btn-begin').removeClass('pt-btn-begin-inactive');
-        $('.pt-btn-begin').toggleClass('pulse-button');
-
         //Reset the app 
         resetWithState(lastState);
 
@@ -520,36 +412,22 @@ resetWithState = (state) => {
     //Initialize the global variables
     if (navigator.onLine) isNetworkOnline = true;
     else isNetworkOnline = false;
-    canGoNext = false;
 
-    currentRoundAnswerIDs = lastState.currentAnswerSelection;
-    if (!currentRoundAnswerIDs) currentRoundAnswerIDs = [0, 0, 0, 0];
+    answerArray = lastState.currentAnswerSelection;
+    if (!answerArray) answerArray = [];
 
-    ROUND_CURRENT_INDEX = lastState.currentRound + 1;
-    Answer_Pass_Index = lastState.currentRound;
-    totalCorrectNum = lastState.currentCorrectNum;
-    current_round_correct_num = 0;
+
+
+    currentRound = lastState.currentRound;
 
     //Update the screens with queries
     updateQuestionsAndAnswers(QuizDetail['Activity129']);
 
     //Go to the corresponding screen
-    PageTransitions.gotoPage(Answer_Pass_Index * 2 + 1);
-
-    //Update the score board
-    $('.pt-score-label .number-score').text(totalCorrectNum * 100);
-
-    //Update the result page
-    if(Answer_Pass_Index >= 3) {
-        //Display the total earned points
-        $('#final-earned-points').text(totalCorrectNum * 100 + ' POINTS');
-
-        //Display the count of correct answers
-        $('#result-text').text(totalCorrectNum + '/12');
-    }
+    
 
     //Set the current round answer section with saved one
-    updateAnswerDropdown(currentRoundAnswerIDs);
+    updateAnswerDropdown(answerArray);
 }
 
 
